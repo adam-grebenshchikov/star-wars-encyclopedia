226bee26e2d6dcd30a475870170b0e87fc05bc4b
Adam Grebenshchikov <adam.grebenshchikov@gmail.com>
2021-08-04 13:44:28 +0400

    feat: added search bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
     <Header />
+    <div class="search-field-wrapper">
+      <SearchField
+        placeholder="Search by Name"
+        width="800px"
+        @input="toSaveSearchValue"
+      />
+    </div>
   </div>
 </template>
 
@@ -8,10 +15,21 @@
 import "reset-css/reset.css";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import Header from "./components/Header.vue";
+import SearchField from "./components/SearchField.vue";
 
 export default {
   name: "App",
-  components: { Header },
+  components: { Header, SearchField },
+  data() {
+    return {
+      searchValue: "",
+    };
+  },
+  methods: {
+    toSaveSearchValue: function (value) {
+      this.searchValue = value;
+    },
+  },
 };
 </script>
 
@@ -22,5 +40,12 @@ export default {
   color: #fff;
   background-color: #333;
   min-height: 100vh;
+}
+
+.search-field-wrapper {
+  display: flex;
+  justify-content: center;
+  padding: 80px 0;
+  margin: 0 32px;
 }
 </style>
---
+++ src/components/SearchField.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="search-field-container" :style="cssProps">
+    <input
+      class="search-field"
+      type="text"
+      :placeholder="placeholder"
+      @input="sendValueToParent"
+    />
+    <button class="search-field-icon">
+      <img src="../assets/search-field-magnifier.svg" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchField",
+  props: {
+    placeholder: String,
+    width: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    sendValueToParent: function (e) {
+      this.$emit("input", e.target.value);
+    },
+  },
+  computed: {
+    cssProps: function () {
+      return {
+        "flex-basis": this.width,
+      };
+    },
+  },
+};
+</script>
+
+
+<style>
+.search-field {
+  color: #808080;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 21px;
+  font-family: inherit;
+  background-color: transparent;
+  border: 0px;
+  border-bottom: 1px solid #808080;
+  padding-bottom: 7.5px;
+  width: 100%;
+}
+
+.search-field-container {
+  display: flex;
+}
+
+.search-field-icon {
+  background: transparent;
+  border: none;
+  outline: none;
+  margin-left: -29px;
+  padding-right: 0;
+}
+</style>
