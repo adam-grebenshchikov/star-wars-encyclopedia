c3706a9f3cfef4c2f835928a59d5daca0c024510
Adam Grebenshchikov <adam.grebenshchikov@gmail.com>
2021-08-05 14:31:47 +0400

    feat: added card list component

---
+++ src/components/CardList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card-list">
+    <CardListItem v-for="person in data" :key="person.url" :person="person" />
+  </div>
+</template>
+
+<script>
+import CardListItem from "./CardListItem.vue";
+export default {
+  name: "CardList",
+  components: {
+    CardListItem,
+  },
+  props: {
+    data: Array,
+  },
+};
+</script>
+
+<style>
+.card-list {
+  display: flex;
+  margin: auto;
+  flex-wrap: wrap;
+  padding: 0 16px;
+}
+</style>
